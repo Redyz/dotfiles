0ca4bbb0c69c65a9328291348c0146b59fc02f49
reigthb <reightb@reightb.com>
2016-02-26 00:32:52 -0500

    Small tmux plugin manager edit

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-echo "Neobundle & dotfiles installing script"
+echo "Dotfiles install script"
 touch ~/.private-bashrc
+
+if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then 
+	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi;
+
 if [ ! -d "$HOME/.vim/bundle/neobundle.vim" ]; then 
 	echo "Installing Neobundle"
 	curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > install-neobundle-script-temp.sh
